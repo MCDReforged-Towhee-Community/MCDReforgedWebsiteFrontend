248286b061ef67660855b3cae6fba80f5d0dfb90
Andy Zhang <37402126+AnzhiZhang@users.noreply.github.com>
2023-02-28 18:25:14 +0000

    feat: 🔧 add html head and set title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    app: {
+        head: {
+            title: "MCDR Plugin Catalogue",
+            meta: [
+                {
+                    name: "viewport",
+                    content: "width=device-width, initial-scale=1"
+                },
+                {charset: "utf-8"},
+            ],
+        },
+    },
+
     nitro: {
         preset: "vercel-edge",
     },
