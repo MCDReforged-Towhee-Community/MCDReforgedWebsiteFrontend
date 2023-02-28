6e2e848f811bb5b2e3fd50aa08b726545859d53a
Andy Zhang <37402126+AnzhiZhang@users.noreply.github.com>
2023-02-28 20:13:19 +0000

    feat: 🔧 add icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,6 +10,13 @@ export default defineNuxtConfig({
                 },
                 {charset: "utf-8"},
             ],
+            link: [
+                {
+                    rel: "icon",
+                    type: "image/x-icon",
+                    href: "https://avatars.githubusercontent.com/u/63280128"
+                },
+            ],
         },
     },
 
