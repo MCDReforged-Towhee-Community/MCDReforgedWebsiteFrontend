8088e4e4fbe473550bbcb9ee564d830bdc22ac16
Andy Zhang <37402126+AnzhiZhang@users.noreply.github.com>
2023-03-01 11:46:51 +0000

    feat(pages): ✨ navigate to /pugins in index

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  middleware: [
+    function () {
+      return navigateTo("/plugins");
+    },
+  ],
+});
+</script>
+
+<style scoped>
+</style>
