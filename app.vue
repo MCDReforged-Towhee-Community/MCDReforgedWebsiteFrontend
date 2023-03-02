5a948723d0d8ab8b734138504c8258ae8358e9b6
Andy Zhang <37402126+AnzhiZhang@users.noreply.github.com>
2023-03-02 15:42:02 +0000

    feat: 🌐 add element-plus i18n

--- app.vue
+++ app.vue
@@ -1,11 +1,24 @@
 <template>
   <NuxtLayout>
-    <NuxtPage/>
+    <ElConfigProvider :locale="locale">
+      <NuxtPage/>
+    </ElConfigProvider>
   </NuxtLayout>
 </template>
 
 <script setup lang="ts">
+import enUS from "element-plus/lib/locale/lang/en";
+import zhCN from "element-plus/lib/locale/lang/zh-cn";
+
 const i18n = useI18n();
+
+// element-plus i18n
+const locale = computed(() => (
+    {
+      "en-US": enUS,
+      "zh-CN": zhCN,
+    }[i18n.locale.value]
+));
 
 // title
 useHead({title: () => i18n.t("global.title")});
