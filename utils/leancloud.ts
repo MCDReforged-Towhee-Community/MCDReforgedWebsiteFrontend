21f9b7ca7cdc6dd68a99d76abb230b4e931b8d90
Andy Zhang <37402126+AnzhiZhang@users.noreply.github.com>
2023-03-06 14:27:37 +0000

    feat(utils): ✨ add createVote and increaseVote in leancloud

--- utils/leancloud.ts
+++ utils/leancloud.ts
@@ -8,20 +8,31 @@ export function fetchData(method: HTTPMethod, url: string, data: object = {}) {
             "X-LC-Key": useRuntimeConfig().public.leanCloud.appKey,
             "Content-Type": "application/json"
         },
+        body: method === "POST" || method === "PUT" ? JSON.stringify(data) : undefined,
+        query: method === "GET" ? data : {},
     });
 }
 
 /**
  * Vote data.
+ * @interface Vote
+ * @property {string} objectId Object id.
+ * @property {string} id Plugin id.
+ * @property {number} vote Vote number.
+ */
+export interface Vote {
+    objectId: string,
+    id: string
+    vote: number,
+}
+
+/**
+ * Vote data in object which key is plugin id.
  * @interface VotesData
  * @property {number} [key] Plugin id with vote number.
  */
 export interface VotesData {
-    [key: string]: {
-        objectId: string,
-        id: string
-        vote: number,
-    }
+    [key: string]: Vote
 }
 
 /**
@@ -29,14 +40,56 @@ export interface VotesData {
  * @returns {Promise<VotesData>} Votes data.
  */
 export async function fetchVotes(id?: string): Promise<VotesData> {
-    interface VotesFetched {
-        results: {
-            objectId: string,
-            id: string,
-            vote: number
-        }[]
+    interface fetchVotesResponse {
+        results: Vote[]
     }
 
-    const votes: VotesFetched = await fetchData("GET", "/classes/Plugins_Votes") as VotesFetched;
+    // query
+    let query = {};
+    if (id !== undefined) {
+        query = {where: {id: id}};
+    }
+
+    // fetch
+    const votes: fetchVotesResponse = await fetchData(
+        "GET",
+        "/classes/Plugins_Votes",
+        query
+    ) as fetchVotesResponse;
+
+    // parse and return
     return Object.fromEntries(votes.results.map(vote => [vote.id, vote]));
+}
+
+/**
+ * Create vote for a plugin in LeanCloud.
+ * @param {string} id Plugin id.
+ */
+export async function createVote(id: string): Promise<Vote> {
+    interface CreateVoteResponse {
+        objectId: string
+    }
+
+    const response: CreateVoteResponse = await fetchData(
+        "POST",
+        "/classes/Plugins_Votes",
+        {id}
+    ) as CreateVoteResponse;
+    return {
+        id: id,
+        vote: 0,
+        ...response
+    }
+}
+
+/**
+ * Increase vote for a plugin in LeanCloud.
+ * @param {string} objectId Object id.
+ */
+export async function increaseVote(objectId: string): Promise<void> {
+    await fetchData(
+        "PUT",
+        `/classes/Plugins_Votes/${objectId}`,
+        {vote: {"__op": "Increment", "amount": 1}}
+    );
 }
