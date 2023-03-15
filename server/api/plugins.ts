c038995ff4f90a9010fb0a8b099769d3d58593ad
Andy Zhang <37402126+AnzhiZhang@users.noreply.github.com>
2023-03-15 19:20:39 +0000

    feat(api): ✨ remove plugins api cache

--- server/api/plugins.ts
+++ server/api/plugins.ts
@@ -8,50 +8,35 @@ import {
     MergedPluginDataSummary,
 } from "~/types/plugins";
 
+async function getPluginCatalogueSummary() {
+    // fetch
+    const mergedPluginDataSummary: MergedPluginDataSummary = {};
+    const zipRaw: Blob = await $fetch("https://codeload.github.com/MCDReforged/PluginCatalogue/zip/refs/heads/meta") as Blob;
+    const zip: JSZip = await new JSZip().loadAsync(await zipRaw.arrayBuffer());
 
-function updatePluginCatalogueSummary() {
-    $fetch("https://codeload.github.com/MCDReforged/PluginCatalogue/zip/refs/heads/meta")
-        .then((res) => (res as Blob).arrayBuffer())
-        .then((res) => new JSZip().loadAsync(res))
-        .then(async (zip) => {
-            // summary
-            const mergedPluginDataSummary: MergedPluginDataSummary = {};
+    // authors
+    const {authors}: AuthorSummary = JSON.parse(await zip.file("PluginCatalogue-meta/authors.json")!.async("string"));
 
-            // authors
-            const {authors}: AuthorSummary = JSON.parse(await zip.file("PluginCatalogue-meta/authors.json")!.async("string"));
+    // merge data
+    const pluginMetaSummary: PluginMetaSummary = JSON.parse(await zip.file("PluginCatalogue-meta/plugins.json")!.async("string"));
+    for (const pluginID in pluginMetaSummary.plugins) {
+        const meta: MetaInfo = pluginMetaSummary.plugins[pluginID];
+        const release: ReleaseSummary = JSON.parse(await zip.file(`PluginCatalogue-meta/${pluginID}/release.json`)!.async("string")) as ReleaseSummary;
+        const info: FormattedPluginInfo = JSON.parse(await zip.file(`PluginCatalogue-meta/${pluginID}/plugin.json`)!.async("string")) as FormattedPluginInfo;
+        mergedPluginDataSummary[pluginID] = {
+            meta: meta,
+            release: release,
+            info: info,
+            authors: meta.authors.map((author) => ({
+                name: author,
+                link: authors[author].link
+            })),
+        };
+    }
 
-            // merge data
-            const pluginMetaSummary: PluginMetaSummary = JSON.parse(await zip.file("PluginCatalogue-meta/plugins.json")!.async("string"));
-            for (const pluginID in pluginMetaSummary.plugins) {
-                const meta: MetaInfo = pluginMetaSummary.plugins[pluginID];
-                const release: ReleaseSummary = JSON.parse(await zip.file(`PluginCatalogue-meta/${pluginID}/release.json`)!.async("string")) as ReleaseSummary;
-                const info: FormattedPluginInfo = JSON.parse(await zip.file(`PluginCatalogue-meta/${pluginID}/plugin.json`)!.async("string")) as FormattedPluginInfo;
-                mergedPluginDataSummary[pluginID] = {
-                    meta: meta,
-                    release: release,
-                    info: info,
-                    authors: meta.authors.map((author) => ({
-                        name: author,
-                        link: authors[author].link
-                    })),
-                };
-            }
-
-            // save to storage
-            await useStorage().setItem("mergedPluginCatalogueSummary", mergedPluginDataSummary);
-
-            // log
-            const authorCount = authors.length;
-            const pluginCount = Object.keys(mergedPluginDataSummary).length;
-            console.log(`Plugin catalogue updated with ${authorCount} authors and ${pluginCount} plugins`);
-
-            // next update
-            setTimeout(updatePluginCatalogueSummary, 1000 * 60 * 10);
-        });
+    return mergedPluginDataSummary;
 }
 
-updatePluginCatalogueSummary();
-
-export default defineEventHandler((): MergedPluginDataSummary => {
-    return useStorage().getItem("mergedPluginCatalogueSummary");
+export default defineEventHandler(async (): Promise<MergedPluginDataSummary> => {
+    return await getPluginCatalogueSummary();
 });
