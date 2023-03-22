b935bb8f5f8ee38b2a86e09231c638cfe123bcd0
Andy Zhang <37402126+AnzhiZhang@users.noreply.github.com>
2023-03-22 23:43:17 +0000

    feat(api): ✨ add pluginsVotes api

---
+++ server/api/pluginsVotes.ts
@@ -0,0 +1,10 @@
+import {PluginVotesSummary} from "~/types/plugins";
+import {useLeanCloud} from "~/composables/useLeanCloud";
+
+export default defineCachedEventHandler(
+    async (): Promise<PluginVotesSummary> => await useLeanCloud().fetchVotes(),
+    {
+        name: "pluginsVotes",
+        maxAge: 60 * 10,
+    }
+);
