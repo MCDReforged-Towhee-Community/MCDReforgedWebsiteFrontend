3d69a7c2b16d2d2a016332f16fd888629e799cf3
Andy Zhang <37402126+AnzhiZhang@users.noreply.github.com>
2023-03-23 13:47:16 +0000

    feat(components): ✨ add TheFooter

--- app.vue
+++ app.vue
@@ -6,6 +6,7 @@
       <ElConfigProvider :locale="locale">
         <NuxtPage/>
       </ElConfigProvider>
+      <TheFooter/>
     </div>
   </NuxtLayout>
 </template>
---
+++ components/the/TheFooter.vue
@@ -0,0 +1,32 @@
+<template>
+  <footer id="footer">
+    <div>
+      {{ t("notice") }}
+    </div>
+    <div>
+      Copyright © 2023 Andy Zhang. All Rights Reserved.
+    </div>
+  </footer>
+</template>
+
+<script setup lang="ts">
+const {t} = useI18n();
+</script>
+
+<style scoped lang="scss">
+#footer {
+  padding: 1rem 0;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
+
+<i18n locale="en-US" lang="yaml">
+notice: Please be aware that this is NOT the official website of MCDReforged or its community.
+</i18n>
+
+<i18n locale="zh-CN" lang="yaml">
+notice: 请注意这不是 MCDReforged 或社区的官方网站。
+</i18n>
