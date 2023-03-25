6354b9d6c67057a2bdd7607354bd5399efcc4935
Andy Zhang <37402126+AnzhiZhang@users.noreply.github.com>
2023-03-25 19:39:09 +0000

    feat(composables): ✨ add useEventListener

---
+++ composables/useEventListener.ts
@@ -0,0 +1,4 @@
+export default function useEventListener(target: EventTarget, type: string, callback: EventListenerOrEventListenerObject | null) {
+    onMounted(() => target.addEventListener(type, callback));
+    onUnmounted(() => target.removeEventListener(type, callback));
+}
