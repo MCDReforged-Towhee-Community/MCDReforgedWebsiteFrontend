2eb82d8c4b0117d3f7f01167d5ec8704d0678a1f
Andy Zhang <37402126+AnzhiZhang@users.noreply.github.com>
2023-06-18 14:56:17 +0800

    feat(components): ✨ add BaseMultilineDiv

---
+++ components/base/BaseMultilineDiv.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div v-for="i in value.split('\n').filter(item => item !== '')">
+      {{ i }}
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const {value} = defineProps<{
+  value: string;
+}>();
+</script>
+
+<style scoped lang="scss">
+
+</style>
