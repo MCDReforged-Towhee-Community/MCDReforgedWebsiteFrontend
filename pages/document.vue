afac7e9e5dccbb981e596e82dd3a7d11f4f2ce54
Andy Zhang <37402126+AnzhiZhang@users.noreply.github.com>
2024-01-12 21:11:16 +0000

    feat(pages): ✨ add /document page

---
+++ pages/document.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="document">
+    <iframe
+        class="iframe"
+        :src="url"
+    />
+    <TheFooter/>
+  </div>
+</template>
+
+<script setup lang="ts">
+const {locale} = useI18n();
+
+const url = computed(() => (
+    {
+      "en-US": "https://mcdreforged.readthedocs.io/en/latest/",
+      "zh-CN": "https://mcdreforged.readthedocs.io/zh-cn/latest/",
+    }[locale.value as string]
+));
+</script>
+
+<style scoped lang="scss">
+@import "assets/css/variables";
+
+#document {
+  height: calc(100vh - 4rem);
+
+  display: flex;
+  flex-direction: column;
+}
+
+.iframe {
+  width: 100%;
+  height: 100%;
+
+  border: none;
+  border-radius: 1rem;
+  box-shadow: $shadow-basic;
+}
+</style>
